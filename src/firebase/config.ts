1ecf29f012fb7f0aa4cc1cc01f9f3b232970de3b
Reidison Lima <reidisonlima@gmail.com>
2025-11-11 05:58:36 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4783918348-ab92d",
+  "appId": "1:951481256415:web:96268eb6cccfcea1ee1c6a",
+  "apiKey": "AIzaSyCARsiO_hUO5TXBgyWYCYM0bBAFPil3j_Y",
+  "authDomain": "studio-4783918348-ab92d.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "951481256415"
+};
