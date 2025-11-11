7d807118fef9d5cf0d98b8226bdfb3edefa5c4ed
Reidison Lima <reidisonlima@gmail.com>
2025-11-11 05:13:26 +0000

    a imagem a logo é a que segue anexa

--- src/components/icons/logo.tsx
+++ src/components/icons/logo.tsx
@@ -4,13 +4,12 @@ import Image from 'next/image';
 export function Logo(props: Omit<React.ComponentProps<typeof Image>, 'src' | 'alt'>) {
   return (
     <Image
-      src="https://picsum.photos/seed/logo/472/184"
+      src="/logo.png"
       alt="Pousada Solar das Gerais"
       width={472}
       height={184}
       priority
       {...props}
-      data-ai-hint="logo"
     />
   );
 }
